415a46ad6e105575dc77807bac8d1257a297d0c4
Mitja Ševerkar <mytja@protonmail.com>
2024-06-29 20:33:05 +0200

    Update evo.mk

--- evo.mk
+++ evo.mk
@@ -1,11 +1,8 @@
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-$(call inherit-product, vendor/evolution/config/BoardConfigSoong.mk)
-$(call inherit-product, device/evolution/sepolicy/common/sepolicy.mk)
--include vendor/evolution/build/core/config.mk
+$(call inherit-product, device/phh/treble/lineage.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
 
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
-SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 BOARD_EXT4_SHARE_DUP_BLOCKS := true
 BUILD_BROKEN_DUP_RULES := true

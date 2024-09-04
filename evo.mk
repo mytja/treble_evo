3c83eaf55bcabb2ae0e33469f45217eb6fdc07ab
mytja <mitja@severkar.eu>
2024-09-04 16:45:08 +0200

    correct OTA URL
    Change-Id: Ia9a9a165a580d276991b8b2fba62dc6405fb3cb8

--- evo.mk
+++ evo.mk
@@ -14,7 +14,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/kaii-lb/treble_everest/14/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/mytja/treble_aosp/main/ota.json
 
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true

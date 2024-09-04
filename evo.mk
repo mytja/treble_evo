1f89401b46b7ef5cc9fd1a9625ae496bfc57d0fc
mytja <mitja@severkar.eu>
2024-09-04 16:44:20 +0200

    add ota
    Change-Id: Ie867cc9ec653ba5e3baaf53dc367ac5fb042c34e

--- evo.mk
+++ evo.mk
@@ -12,6 +12,10 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 #BUILD_BROKEN_DUP_RULES := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# OTA
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/kaii-lb/treble_everest/14/ota.json
+
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true

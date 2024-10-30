49349a3507628b28881b0c8654061943b5e3271b
mytja <mitja@severkar.eu>
2024-10-30 01:01:58 +0100

    Add manufacturer, clean up makefile
    Change-Id: Id392cb774ab0cdb3063df2a5b96fbdf8943aea3b

--- evo.mk
+++ evo.mk
@@ -2,7 +2,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 
-override PRODUCT_MODEL := Evolution X GSI
+PRODUCT_DEVICE := arm64_bgN
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MANUFACTURER := google
+PRODUCT_SYSTEM_MANUFACTURER := google
+PRODUCT_MODEL := Evolution X GSI
 
 # No kernel image
 TARGET_NO_KERNEL_OVERRIDE := true
@@ -12,16 +17,15 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 #BUILD_BROKEN_DUP_RULES := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_FACE_UNLOCK := true
-override TARGET_SUPPORTS_64_BIT_APPS := true
+# To enable Face Unlock
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 # OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/mytja/treble_evo/main/ota.json
 
 # SELinux
-SELINUX_IGNORE_NEVERALLOWS := true
+#SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 
 # Evolution X
@@ -29,8 +33,7 @@ EVO_BUILD_TYPE := Unofficial
 
 # Additional packages
 PRODUCT_PACKAGES += \
-  OpenEUICC \
-  FaceUnlockService
+  OpenEUICC
 
 # Evolution X maintainer overlay
 #DEVICE_PACKAGE_OVERLAYS += \
